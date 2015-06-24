2af3c273d9751c3407ee4b106022311632321b43
李伟宾 <dylan_lwb@163.com>
2015-06-24 12:52:01 +0800

    Initial Commit

---
+++ WBImageCarousel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WBImageCarousel
+//
+//  Created by 李伟宾 on 15/6/24.
+//  Copyright (c) 2015年 李伟宾. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
